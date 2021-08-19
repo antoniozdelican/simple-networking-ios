e40b4c8617a3fa906da2d739fdc914a2e81f7b7e
Antonio Zdelican <antonio.zdelican@sunhill-technologies.com>
2021-08-19 11:22:04 +0200

    Added initial pod files

---
+++ SimpleNetworking/Example/SimpleNetworking/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SimpleNetworking
+//
+//  Created by antoniozdelican on 08/19/2021.
+//  Copyright (c) 2021 antoniozdelican. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
